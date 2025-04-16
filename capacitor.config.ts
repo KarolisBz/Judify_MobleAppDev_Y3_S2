613d4e9cd6050dc05d0577d6e13fe8c305038df0
Karolis Bandziulis <G00417529@atu.ie>
2025-04-17 00:08:54 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'judofy',
+  webDir: 'www'
+};
+
+export default config;
