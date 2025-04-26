dddaad98259323abdf14133ca55cbe4faa2bac06
Karolis Bandziulis <G00417529@atu.ie>
2025-04-26 03:36:51 +0100

    fixed some scaling issues

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonImg, IonModal } from '@ionic/angular/standalone';
+import { IonContent, IonTitle, IonGrid, IonRow, IonCol, IonImg } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonGrid, IonRow, IonCol, IonImg, IonModal]
+  imports: [IonContent, IonTitle, CommonModule, FormsModule, IonGrid, IonRow, IonCol, IonImg]
 })
 export class HomePage implements OnInit {
 
