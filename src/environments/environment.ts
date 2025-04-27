abbe12b79e5645bbb8bb1632d22a8703e09fb528
Karolis Bandziulis <G00417529@atu.ie>
2025-04-27 07:44:48 +0100

    Imported Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAEGQcziLkYrkPN4zq4Ilm_1wn-z-EnrEI",
+    authDomain: "judofy.firebaseapp.com",
+    projectId: "judofy",
+    storageBucket: "judofy.firebasestorage.app",
+    messagingSenderId: "759467777624",
+    appId: "1:759467777624:web:ed0141bb129ad1b2d9742d",
+    measurementId: "G-19P5CJM2DG"
+  }
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+// Firebase imports
+import { initializeApp } from 'firebase/app';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';;
+
+// enable production mode for preformance
+if (environment.production) {
+  enableProdMode();
+}
+
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 });
 
